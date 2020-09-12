98980929031f696841ecab098bcbd37f4a6b3e62
redchrysler <RedChrysler@protonmail.com>
2020-09-12 21:37:09 +0300

    Finish base table logic

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,112 +1,112 @@
 class Dom {
-  constructor(selector) {
-    this.$el = typeof selector === 'string' ? document.querySelector(selector) : selector
-  }
-
-  html(html) {
-    if (typeof html === 'string') {
-      this.$el.innerHTML = html
-      return this
-    }
-    return this.$el.outerHTML.trim()
-  }
-  text(text) {
-    if (typeof text === 'string') {
-      this.$el.textContent = text
-      return this
-    }
-    if (this.$el.tagName.toLoweCase() === 'input') {
-      return this.$el.value.trim();
-    }
-    return this.$el.textContent.trim();
-  }
-
-  clear() {
-    this.html('')
-    return this
-  }
-
-  // Аналог addEventListener
-  on(eventType, callback) {
-    this.$el.addEventListener(eventType, callback)
-  }
-
-  off(eventType, callback) {
-    this.$el.removeEventListener(eventType, callback)
-  }
-
-  append(node) {
-    if (node instanceof Dom) {
-      node = node.$el
-    }
-    if (Element.prototype.append) {
-      this.$el.append(node)
-    } else {
-      this.$el.appendChild(node)
+    constructor(selector) {
+        this.$el = typeof selector === 'string' ? document.querySelector(selector) : selector
     }
 
-    return this
-  }
-
-  get data() {
-    return this.$el.dataset
-  }
-
-  closest(selector) {
-    return $(this.$el.closest(selector))
-  }
-
-  getCoords() {
-    return this.$el.getBoundingClientRect()
-  }
-
-  find(selector) {
-    return $(this.$el.querySelector(selector))
-  }
-
-  findAll(selector) {
-    return this.$el.querySelectorAll(selector)
-  }
-
-  addClass(className) {
-    this.$el.classList.add(className)
-    return this
-  }
-
-  removeClass(className) {
-    this.$el.classList.remove(className)
-    return this
-  }
-
-  css(styles = {}) {
-    Object.keys(styles).forEach( (key) => {
-      this.$el.style[key] = styles[key]
-    })
-  }
-  id(parse) {
-    if (parse) {
-      const parsed = this.id().split(":")
-      return {
-        row: +parsed[0],
-        col: +parsed[1]
-      }
+    html(html) {
+        if (typeof html === 'string') {
+            this.$el.innerHTML = html
+            return this
+        }
+        return this.$el.outerHTML.trim()
     }
-    return this.data.id
-  }
-  focus() {
-    this.$el.focus()
-    return this
-  }
+    text(text) {
+        if (typeof text === 'string') {
+            this.$el.textContent = text
+            return this
+        }
+        if (this.$el.tagName.toLoweCase() === 'input') {
+            return this.$el.value.trim();
+        }
+        return this.$el.textContent.trim();
+    }
+
+    clear() {
+        this.html('')
+        return this
+    }
+
+    // Аналог addEventListener
+    on(eventType, callback) {
+        this.$el.addEventListener(eventType, callback)
+    }
+
+    off(eventType, callback) {
+        this.$el.removeEventListener(eventType, callback)
+    }
+
+    append(node) {
+        if (node instanceof Dom) {
+            node = node.$el
+        }
+        if (Element.prototype.append) {
+            this.$el.append(node)
+        } else {
+            this.$el.appendChild(node)
+        }
+
+        return this
+    }
+
+    get data() {
+        return this.$el.dataset
+    }
+
+    closest(selector) {
+        return $(this.$el.closest(selector))
+    }
+
+    getCoords() {
+        return this.$el.getBoundingClientRect()
+    }
+
+    find(selector) {
+        return $(this.$el.querySelector(selector))
+    }
+
+    findAll(selector) {
+        return this.$el.querySelectorAll(selector)
+    }
+
+    addClass(className) {
+        this.$el.classList.add(className)
+        return this
+    }
+
+    removeClass(className) {
+        this.$el.classList.remove(className)
+        return this
+    }
+
+    css(styles = {}) {
+        Object.keys(styles).forEach( (key) => {
+            this.$el.style[key] = styles[key]
+        })
+    }
+    id(parse) {
+        if (parse) {
+            const parsed = this.id().split(":")
+            return {
+                row: +parsed[0],
+                col: +parsed[1]
+            }
+        }
+        return this.data.id
+    }
+    focus() {
+        this.$el.focus()
+        return this
+    }
 }
 // event.target
 export function $(selector) {
-  return new Dom(selector)
+    return new Dom(selector)
 }
 
 $.create = (tagName, classes = '') => {
-  const el = document.createElement(tagName)
-  if (classes) {
-    el.classList.add(classes)
-  }
-  return $(el)
+    const el = document.createElement(tagName)
+    if (classes) {
+        el.classList.add(classes)
+    }
+    return $(el)
 }
